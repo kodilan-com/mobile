3b295b86bdc15cd83919a1e98c4e75dfc1e2d2c4
orcuntuna <turboindirblog@gmail.com>
2020-05-03 12:25:24 +0300

    fixed 404 svg problem

--- src/components/CompanyImage.js
+++ src/components/CompanyImage.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import {Image} from 'react-native';
-import {SvgUri} from 'react-native-svg';
+import React, {useState, useEffect} from 'react';
+import {Image, Text} from 'react-native';
+import {SvgUri, SvgXml} from 'react-native-svg';
+import getSvgData from '../requests/getSvgData';
 
 function CompanyImage({uri, width, height, style}) {
-  if (uri.includes('.svg')) {
-    return <SvgUri style={style} width={width} height={height} uri={uri} />;
-  } else {
-    return <Image style={style} source={{uri}} />;
-  }
+  const [svg, setSvg] = useState();
+  useEffect(() => {
+    if (uri.includes('.svg')) {
+      getSvgData(uri).then(res => {
+        setSvg(res.data);
+      });
+    }
+  }, []);
+  return (
+    <>
+      {uri.includes('.svg') ? (
+        svg ? (
+          <SvgXml style={style} width={width} height={height} xml={svg} />
+        ) : (
+          <Image
+            style={style}
+            source={{
+              uri: 'https://kodilan.com/img/empty-company-logo.8437254b.png',
+            }}
+          />
+        )
+      ) : (
+        <Image style={style} source={{uri}} />
+      )}
+    </>
+  );
 }
 
 export default CompanyImage;
---
+++ src/requests/getSvgData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const getSvgData = (url) => {
+  return new Promise((resolve, reject) => {
+    const request_url = url;
+    axios({
+      method: 'GET',
+      url: request_url,
+    })
+      .then(res => {
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+export default getSvgData;
